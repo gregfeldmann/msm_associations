92bcbf378cd17fe342de940660dd16af176fa23e
Greg Feldmann <gregory.j.feldmann@chicagobooth.edu>
2017-07-28 21:38:37 +0000

    commit friday

---
+++ app/models/director.rb
@@ -0,0 +1,28 @@
+# == Schema Information
+#
+# Table name: directors
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  dob        :string
+#  bio        :text
+#  image_url  :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Director < ApplicationRecord
+
+ has_many(:movies, :class_name => "Movies", :foreign_key => "movie_id")
+ 
+ # name: must be present; must be unique in combination with dob
+  validates :name, :presence => true, :uniqueness => { :scope => :dob }
+  
+ # dob: no rules
+ # bio: no rules
+ # image_url: no rules    
+    
+    
+    
+    
+end
